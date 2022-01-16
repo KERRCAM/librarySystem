7a50205d49b5e8a264a06371ff501fdb81a29ae8
KERRCAM <kerrcameron@hotmail.com>
2022-01-16 23:54:36 +0000

    comments

--- src/com/company/bookLoanFunctions.java
+++ src/com/company/bookLoanFunctions.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class bookLoanFunctions {
 
+// new file for loans - use seach to get book - remove book function - add book plus user to borrowed - return just search borrow file for book confirm user - remove form borrow add to books again
 
 
 
